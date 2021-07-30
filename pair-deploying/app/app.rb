d0849ef3e3f365b5cb970a10a39b30cf25400d1a
Matthieu Rosinski <korrigan@slash16.org>
2021-07-30 14:24:40 +0200

    added deploy app

--- pair-deploying/app/app.rb
+++ pair-deploying/app/app.rb
@@ -1,7 +1,62 @@
 require 'rack'
+require 'json'
 
 class App
   def call env
-    [200, {"Content-Type": "text/plain"}, "Hello World!"]
+    req = Rack::Request.new env
+    case req.path
+    when '/'
+      index req, env
+    when '/api'
+      root req, env
+    when '/api/stuff'
+      stuff req, env
+    else
+      not_found req, env
+    end
+  end
+
+  def index _req, env
+    [
+      200,
+      {"Content-Type" => "text/html"},
+      File.open('index.html', File::RDONLY)
+    ]
+  end
+
+  def root _req, _env
+    [
+      200,
+      {"Content-Type" => "application/json"},
+      [JSON.dump({hello: :world})]
+    ]
+  end
+
+  def stuff _req, _env
+    resp = {
+      stuff: [
+        {
+          id: 1,
+          name: 'thing1'
+        },
+        {
+          id: 2,
+          name: 'thing2'
+        }
+      ]
+    }
+    [
+      200,
+      {"Content-Type" => "application/json"},
+      [JSON.dump(resp)]
+    ]
+  end
+
+  def not_found _req, _env
+    [
+      404,
+      {"Content-Type" => "application/json"},
+      [JSON.dump({error: "not found"})]
+    ]
   end
 end
